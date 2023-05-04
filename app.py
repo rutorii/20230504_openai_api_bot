635dd44f231ad4c90932fcd592a9afc26044606d
woodsub <66902734+woodsub@users.noreply.github.com>
2023-05-04 17:05:44 +0900

    Update app.py

--- app.py
+++ app.py
@@ -8,9 +8,11 @@ openai.api_key = st.secrets.OpenAIAPI.openai_api_key
 # st.session_stateを使いメッセージのやりとりを保存
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
-        {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+        {"role": "system", "content": st.secrets.AppSettings.chatbot_setting}
         ]
 
+    # {"role": "system", "content": "あなたは優秀なアシスタントAIです。"}
+    
 # チャットボットとやりとりする関数
 def communicate():
     messages = st.session_state["messages"]
